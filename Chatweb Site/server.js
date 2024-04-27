0bea1f0fc51c70e685358dfe93002e2700142d91
LiamLiamLiam123 <163282026+LiamLiamLiam123@users.noreply.github.com>
2024-04-27 20:24:21 +1000

    Add files via upload

---
+++ Chatweb Site/server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Page not found');
+    }
+});
+
+const PORT = 3000;
+const IP_ADDRESS = '192.168.15.1';
+
+server.listen(PORT, IP_ADDRESS, () => {
+    console.log(`Server running at http://${IP_ADDRESS}:${PORT}/`);
+});
